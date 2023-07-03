18b5853ee68b8001205302841b4791e5f05bc7cb
MandyCyber <MandyCyber@users.noreply.github.com>
2023-07-03 11:16:30 -0400

    updating setup.py

--- setup.py
+++ setup.py
@@ -2,14 +2,8 @@ from setuptools import setup, find_packages
 import codecs
 import os
 
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
-
 VERSION = '1.0.0'
 DESCRIPTION = 'Web scraper for subreddits'
-LONG_DESCRIPTION = 'A simple way to scrape titles, body text, and more from posts on subreddits'
 
 # Setting up
 setup(
@@ -17,10 +11,9 @@ setup(
     version=VERSION,
     author="Mandy-cyber",
     author_email="",
-    description=DESCRIPTION,
-    long_description_content_type="text/markdown",
-    long_description=long_description,
+    description=DESCRIPTION,\
     packages=find_packages(),
+    url="https://github.com/Mandy-cyber/sreddit",
     install_requires=['selenium', 'get_chrome_driver', 'pysqlite3'],
     keywords=['python', 'selenium', 'reddit', 'subreddit'],
     classifiers=[
